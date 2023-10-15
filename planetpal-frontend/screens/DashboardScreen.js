44a3d9ae64de78833fff93c35a5141a88c588293
markestiller <markalestiller@gmail.com>
2023-10-15 04:36:57 -0400

    minor errors in redeem

--- planetpal-frontend/screens/DashboardScreen.js
+++ planetpal-frontend/screens/DashboardScreen.js
@@ -1,49 +1,64 @@
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { Text, View, StyleSheet, Alert, Dimensions } from 'react-native';
-import { Card, Title, Paragraph, ProgressBar, List, Avatar } from 'react-native-paper';
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { Text, View, StyleSheet, Alert, Dimensions } from "react-native";
+import {
+  Card,
+  Title,
+  Paragraph,
+  ProgressBar,
+  List,
+  Avatar,
+} from "react-native-paper";
 
 const DashboardScreen = ({ navigation }) => {
+  const [currentProgress, setCurrentProgress] = useState(0);
 
   const badges = [
     {
-      name: 'Eco Champion',
-      description: 'Demonstrate excellent recycling habits.',
-      icon: 'earth',
+      name: "Eco Champion",
+      description: "Demonstrate excellent recycling habits.",
+      icon: "earth",
     },
     {
-      name: 'Green Warrior',
-      description: 'Consistently recycle a diverse range of materials.',
-      icon: 'tree',
+      name: "Green Warrior",
+      description: "Consistently recycle a diverse range of materials.",
+      icon: "tree",
     },
   ];
 
   const badges2 = [
     {
-      name: 'Ocean Protector',
-      description: 'Commitment to reducing plastic waste and promoting ocean conservation.',
-      icon: 'waves',
+      name: "Ocean Protector",
+      description:
+        "Commitment to reducing plastic waste and promoting ocean conservation.",
+      icon: "waves",
     },
-  ]
+  ];
 
   React.useLayoutEffect(() => {
     navigation.setOptions({
       headerStyle: {
-        backgroundColor: '#3D550C'
+        backgroundColor: "#3D550C",
       },
-      headerTintColor: '#fff',
+      headerTintColor: "#fff",
     });
   }, [navigation]);
 
   return (
     <View style={styles.container}>
-      
-
       <Card style={styles.card}>
         <Card.Content>
-          <Title style={styles.cardTitle}><Text style={{ color: "#3D550C" }}>Green</Text> Tokens</Title>
-          <Title style={styles.progressTitle}>Green Challenge Progress</Title><Text style={styles.cardParagraph}>October 2023</Text>
-          <ProgressBar progress={0.2} color="#81b622" visible={true} style={styles.progressBar} />
+          <Title style={styles.cardTitle}>
+            <Text style={{ color: "#3D550C" }}>Green</Text> Tokens
+          </Title>
+          <Title style={styles.progressTitle}>Green Challenge Progress</Title>
+          <Text style={styles.cardParagraph}>October 2023</Text>
+          <ProgressBar
+            animatedValue={currentProgress}
+            color="#81b622"
+            visible={true}
+            style={styles.progressBar}
+          />
           {/* <Paragraph style={styles.cardParagraph}>
             WALLALALAa
           </Paragraph> */}
@@ -53,25 +68,31 @@ const DashboardScreen = ({ navigation }) => {
         <Card.Content>
           <Title style={styles.cardTitle}>Badges</Title>
           <List.Section>
-        {badges.map((badge, index) => (
-          <List.Item
-            key={index}
-            title={badge.name}
-            titleStyle={{fontWeight:'bold'}}
-            description={badge.description}
-            left={() => <Avatar.Icon size={58} icon={badge.icon} />}
-          />
-        ))}
-        {badges2.map((badge, index) => (
-          <List.Item
-            key={index}
-            title={badge.name}
-            titleStyle={{fontWeight:'bold'}}
-            description={badge.description}
-            left={() => <Avatar.Icon size={58} icon={badge.icon} style={{backgroundColor: '#cccccc'}}/>}
-          />
-        ))}
-      </List.Section>
+            {badges.map((badge, index) => (
+              <List.Item
+                key={index}
+                title={badge.name}
+                titleStyle={{ fontWeight: "bold" }}
+                description={badge.description}
+                left={() => <Avatar.Icon size={58} icon={badge.icon} />}
+              />
+            ))}
+            {badges2.map((badge, index) => (
+              <List.Item
+                key={index}
+                title={badge.name}
+                titleStyle={{ fontWeight: "bold" }}
+                description={badge.description}
+                left={() => (
+                  <Avatar.Icon
+                    size={58}
+                    icon={badge.icon}
+                    style={{ backgroundColor: "#cccccc" }}
+                  />
+                )}
+              />
+            ))}
+          </List.Section>
         </Card.Content>
       </Card>
 
@@ -82,24 +103,24 @@ const DashboardScreen = ({ navigation }) => {
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#81B622',
+    backgroundColor: "#81B622",
     flex: 1,
     padding: 16,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   progressBar: {
-    alignItems: 'center',
+    alignItems: "center",
     height: 30,
     marginBottom: 10,
   },
   card: {
-    maxHeight:500,
-    width: Dimensions.get('window').width*0.8,
-    backgroundColor: '#fff',
+    maxHeight: 500,
+    width: Dimensions.get("window").width * 0.8,
+    backgroundColor: "#fff",
     marginBottom: 40,
     borderRadius: 15,
-    shadowColor: '#000',
+    shadowColor: "#000",
     shadowOffset: {
       width: 0,
       height: 2,
@@ -110,17 +131,17 @@ const styles = StyleSheet.create({
   },
   cardTitle: {
     fontSize: 30,
-    fontWeight: 'bold',
-    textAlign: 'center',
+    fontWeight: "bold",
+    textAlign: "center",
   },
   progressTitle: {
     fontSize: 20,
-    fontWeight: 'bold',
-    textAlign: 'center',
+    fontWeight: "bold",
+    textAlign: "center",
   },
   cardParagraph: {
-    textAlign: 'center',
-    marginBottom: 6
+    textAlign: "center",
+    marginBottom: 6,
   },
 });
 
